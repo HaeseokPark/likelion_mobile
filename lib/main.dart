336fc37ff6af0e1244b781c8a32bfcb6760ccc4e
LukeSeobyungju <22100356@handong.ac.kr>
2025-06-03 15:47:04 +0900

    calendar

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:likelion/detail.dart';
 import 'package:likelion/home.dart';
 import 'package:likelion/userlist.dart';
 import 'firebase_options.dart';
 import 'login.dart'; 
 import 'register_meeting_page.dart';
-// 예를 들어 메인 화면을 GroupDetailPage로 설정한다고 가정
+import 'calendar.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/detail', // 초기 라우트는 로그인
+      initialRoute: '/calender', // 초기 라우트는 로그인
       routes: {
         '/': (context) => const LoginPage(),
         '/login': (BuildContext context) => const LoginPage(),
@@ -31,6 +30,7 @@ class MyApp extends StatelessWidget {
         '/register': (BuildContext context) => RegisterMeetingPage(), 
         '/user': (BuildContext context) => UserListPage(),
         '/detail': (BuildContext context) => DetailPage(),
+        '/calendar' : (BuildContext context) => CalendarPage(),
       },
     );
   }
